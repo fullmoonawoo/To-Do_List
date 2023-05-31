217adf9d8490f431ffddc19b0519feecd78e29a7
fullmoonawoo <distoredsubsaw@gmail.com>
2023-05-31 15:14:04 +0200

    day end 31.5.2023

--- frontend.py
+++ frontend.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 
 class MainWindow:
@@ -7,32 +8,42 @@ class MainWindow:
         self.window = tk.Tk()
         self.sc_width = self.window.winfo_screenwidth()
         self.sc_height = self.window.winfo_screenheight()
-        self.x = ((self.sc_width / 2) - 200) + (self.sc_width / 4)
-        self.y = ((self.sc_height / 2) - 200) - (self.sc_height / 4)
-        self.window.geometry(('%dx%d+%d+%d' % (620, 280, self.x, self.y)))
+        self.x = ((self.sc_width / 2) - 220) + (self.sc_width / 4)
+        self.y = ((self.sc_height / 2) - 260) - (self.sc_height / 4)
+        self.window.geometry(('%dx%d+%d+%d' % (690, 280, self.x, self.y)))
         self.window.title("Lukas Rausa - TO-DO LIST")
 
-        self.header = tk.Frame(self.window, width=600, height=280, bg="gray8")
-        self.header.grid(row=0, column=0, sticky="WE", ipady=4)
-        self.title = tk.Label(self.header, text=">> TO-DO LIST >>", font=("Source Code Pro", 12, "bold"), fg="green2", bg="gray8")
-        self.title.grid(row=0, column=0)
-        self.add_task = tk.Button(self.header, text="+", font=("Source Code Pro", 14, "bold"), fg="green2", bg="gray24")
-        self.add_task.grid(row=0, column=1, sticky="E")
+        self.header = tk.Frame(self.window, width=600, height=300, bg="gray8")
+        self.header.grid(row=0, column=0, sticky="WE")
+        self.title = tk.Label(self.header, text=">> TO-DO LIST >>", font=("Source Code Pro", 12, "bold"), fg="green2", bg="gray8", anchor="w")
+        self.title.grid(row=0, column=0, ipadx=224)
+        self.add_task = tk.Button(self.header, width=4, text="+", font=("Source Code Pro", 12, "bold"), fg="green2", bg="gray40")
+        self.add_task.grid(row=0, column=1)
 
         self.columns_frame = tk.Frame(self.window, width=620, height=280, bg="gray8")
         self.columns_frame.grid(row=1, column=0, sticky="WE")
 
-        self.tasks = tk.Label(self.columns_frame, text="Task", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
-        self.tasks.grid(row=0, column=0, ipadx=122, padx=1)
-        self.deadline = tk.Label(self.columns_frame, text="Deadline", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
-        self.deadline.grid(row=0, column=1, ipadx=10, padx=1)
-        self.progress = tk.Label(self.columns_frame, text="Progress", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
-        self.progress.grid(row=0, column=2, ipadx=30, padx=1)
-        self.confirmation = tk.Label(self.columns_frame, text="Confirm", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
-        self.confirmation.grid(row=0, column=3, ipadx=10, padx=1)
+        self.tasks = tk.Label(self.columns_frame, width=33, text="Task", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
+        self.tasks.grid(row=0, column=0, padx=1, sticky="W")
+        self.deadlines = tk.Label(self.columns_frame, width=13, text="Deadline", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
+        self.deadlines.grid(row=0, column=1, padx=1, sticky="W")
+        self.progresses = tk.Label(self.columns_frame, width=16, text="Progress", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
+        self.progresses.grid(row=0, column=2, padx=1, sticky="W")
+        self.confirmations = tk.Label(self.columns_frame, width=11, text="Confirm", font=("Source Code Pro", 11, "bold"), fg="green2", bg="gray24")
+        self.confirmations.grid(row=0, column=3, padx=1, sticky="W")
 
-        self.tasks_list = tk.Frame(self.window, width=640, height=280, bg="gray8")
-        self.tasks_list.grid(row=2, column=0)
+        #self.tasks_list = tk.Frame(self.window, width=640, height=250, bg="gray8")
+        #self.tasks_list.grid(row=2, column=0, sticky="W")
+
+        self.task = tk.Label(self.columns_frame, width=37, text="Pošli email do Gewisu teraz hned", font=("Source Code Pro", 8), fg="white",
+                             bg="gray44")
+        self.task.grid(row=1, column=0, padx=1, sticky="WE")
+        self.deadline = tk.Label(self.columns_frame, width=14, text="2.6.2023", font=("Source Code Pro", 8), fg="white", bg="gray44")
+        self.deadline.grid(row=1, column=1, padx=1, sticky="WE", ipadx=1)
+        self.progress = ttk.Progressbar(self.columns_frame, orient="horizontal", mode="indeterminate", length=150)
+        self.progress.grid(row=1, column=2, padx=1, sticky="WE")
+        self.confirmation = tk.Button(self.columns_frame, width=10, text="√", font=("Source Code Pro", 8), fg="white", bg="gray44")
+        self.confirmation.grid(row=1, column=3, padx=1, sticky="WE")
 
     def unpack_tasks(self):
         pass
