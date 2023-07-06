64763bc38d7fed8d233b9e4ba784c68380215b62
fullmoonawoo <distoredsubsaw@gmail.com>
2023-07-06 16:26:59 +0200

    backend: set automaticaly ending hour as 12:00 of target day

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import strftime
 
 
@@ -8,5 +8,6 @@ def refresh_progress(deadline):
     start = datetime.strptime(current_date, "%Y/%m/%d")
     start_hour = datetime.strptime(current_hour, "%H")
     end = datetime.strptime(deadline, "%Y/%m/%d")
+    end = end + timedelta(hours=12)
     delta = (end - start).days * 24 - start_hour.hour
     return delta
