43f64dc47cc780aee92fd85ccdd7baf0e022a29c
fullmoonawoo <distoredsubsaw@gmail.com>
2023-07-11 13:56:08 +0200

    backend: corrected function which should automaticaly set finish of task at 12:00 in taget day

--- backend.py
+++ backend.py
@@ -3,11 +3,9 @@ from time import strftime
 
 
 def refresh_progress(deadline):
-    current_date = strftime("%Y/%m/%d")
-    current_hour = strftime("%H")
-    start = datetime.strptime(current_date, "%Y/%m/%d")
-    start_hour = datetime.strptime(current_hour, "%H")
+    current_date = strftime("%Y/%m/%d %H:%M")
+    start = datetime.strptime(current_date, "%Y/%m/%d %H:%M")
     end = datetime.strptime(deadline, "%Y/%m/%d")
-    end = end + timedelta(hours=12)
-    delta = (end - start).days * 24 - start_hour.hour
+    end += timedelta(hours=12)
+    delta = (end - start)
     return delta
